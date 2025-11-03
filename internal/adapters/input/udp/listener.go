abcefa9e6f8b11a8b6447c7f7be0a095e21a66b2
Denis Beresnev <shanthi.bunch@gmail.com>
2025-11-03 15:05:49 +0300

    feat: custom fields in listener

--- internal/adapters/input/udp/listener.go
+++ internal/adapters/input/udp/listener.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net"
+	"strings"
 	"time"
 
 	"github.com/shanth1/gotools/log"
@@ -56,14 +57,53 @@ func (l *Listener) Start(ctx context.Context) {
 				continue
 			}
 
-			var msg domain.LogMessage
-			if err := json.Unmarshal(buffer[:n], &msg); err != nil {
-				logger.Warn().Err(err).Msg("failed to unmarshal log")
+			var rawData map[string]interface{}
+			if err := json.Unmarshal(buffer[:n], &rawData); err != nil {
+				logger.Warn().Err(err).Str("raw_log", string(buffer[:n])).Msg("failed to unmarshal raw log data")
 				continue
 			}
 
+			msg := domain.LogMessage{
+				Fields: make(map[string]interface{}),
+			}
+
+			if t, ok := rawData["time"]; ok {
+				if timeStr, ok := t.(string); ok {
+					parsedTime, err := time.Parse(time.RFC3339Nano, timeStr)
+					if err != nil {
+						parsedTime, _ = time.Parse(time.RFC3339, timeStr)
+					}
+					msg.Time = parsedTime
+				}
+				delete(rawData, "time")
+			}
 			if msg.Time.IsZero() {
 				msg.Time = time.Now()
+			}
+
+			if lvl, ok := rawData["level"]; ok {
+				msg.Level = fmt.Sprintf("%v", lvl)
+				delete(rawData, "level")
+			}
+			msg.Level = strings.ToLower(msg.Level)
+
+			if app, ok := rawData["app"]; ok {
+				msg.App = fmt.Sprintf("%v", app)
+				delete(rawData, "app")
+			}
+
+			if srv, ok := rawData["service"]; ok {
+				msg.Service = fmt.Sprintf("%v", srv)
+				delete(rawData, "service")
+			}
+
+			if m, ok := rawData["message"]; ok {
+				msg.Message = fmt.Sprintf("%v", m)
+				delete(rawData, "message")
+			}
+
+			for key, value := range rawData {
+				msg.Fields[key] = value
 			}
 
 			if msg.App == "" {
